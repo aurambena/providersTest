bd2ddcb9d3547e1fed71150bc70df8b9d40e6194
Aura Berdugo <amnaranjo27@gmail.com>
2025-07-03 22:08:29 -0500

    singin working with google and facebook

--- auth.ts
+++ auth.ts
@@ -1,40 +1,9 @@
 import NextAuth from "next-auth"
 import "next-auth/jwt"
-import EmailProvider from "next-auth/providers/email"
 
-
-import Apple from "next-auth/providers/apple"
-// import Atlassian from "next-auth/providers/atlassian"
-import Auth0 from "next-auth/providers/auth0"
-import AzureB2C from "next-auth/providers/azure-ad-b2c"
-import BankIDNorway from "next-auth/providers/bankid-no"
-import BoxyHQSAML from "next-auth/providers/boxyhq-saml"
-import Cognito from "next-auth/providers/cognito"
-import Coinbase from "next-auth/providers/coinbase"
-import Discord from "next-auth/providers/discord"
-import Dropbox from "next-auth/providers/dropbox"
 import Facebook from "next-auth/providers/facebook"
-import GitHub from "next-auth/providers/github"
-import GitLab from "next-auth/providers/gitlab"
+
 import Google from "next-auth/providers/google"
-import Hubspot from "next-auth/providers/hubspot"
-import Keycloak from "next-auth/providers/keycloak"
-import LinkedIn from "next-auth/providers/linkedin"
-import MicrosoftEntraId from "next-auth/providers/microsoft-entra-id"
-import Netlify from "next-auth/providers/netlify"
-import Okta from "next-auth/providers/okta"
-import Passage from "next-auth/providers/passage"
-import Passkey from "next-auth/providers/passkey"
-import Pinterest from "next-auth/providers/pinterest"
-import Reddit from "next-auth/providers/reddit"
-import Slack from "next-auth/providers/slack"
-import Salesforce from "next-auth/providers/salesforce"
-import Spotify from "next-auth/providers/spotify"
-import Twitch from "next-auth/providers/twitch"
-import Twitter from "next-auth/providers/twitter"
-import Vipps from "next-auth/providers/vipps"
-import WorkOS from "next-auth/providers/workos"
-import Zoom from "next-auth/providers/zoom"
 import { createStorage } from "unstorage"
 import memoryDriver from "unstorage/drivers/memory"
 import vercelKVDriver from "unstorage/drivers/vercel-kv"
@@ -54,68 +23,19 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   debug: !!process.env.AUTH_DEBUG,
   theme: { logo: "https://authjs.dev/img/logo-sm.png" },
   adapter: UnstorageAdapter(storage),
-  providers: [GitHub({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
+  providers: [
     Google({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
   }),
-  // EmailProvider({
-  //     server: process.env.EMAIL_SERVER,
-  //     from: process.env.EMAIL_FROM,
-  //   }),
+  Facebook({
+    clientId: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET
+  })
+ 
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  //  session: { strategy: "database" }, // Required for email magic links
-    // Apple,
-    // // Atlassian,
-    // Auth0,
-    // AzureB2C,
-    // BankIDNorway,
-    // BoxyHQSAML({
-    //   clientId: "dummy",
-    //   clientSecret: "dummy",
-    //   issuer: process.env.AUTH_BOXYHQ_SAML_ISSUER,
-    // }),
-    // Cognito,
-    // Coinbase,
-    // Discord,
-    // Dropbox,
-    // Facebook,
-    // GitHub,
-    // GitLab,
-    // Google,
-    // Hubspot,
-    // Keycloak({ name: "Keycloak (bob/bob)" }),
-    // LinkedIn,
-    // MicrosoftEntraId,
-    // Netlify,
-    // Okta,
-    // Passkey({
-    //   formFields: {
-    //     email: {
-    //       label: "Username",
-    //       required: true,
-    //       autocomplete: "username webauthn",
-    //     },
-    //   },
-    // }),
-    // Passage,
-    // Pinterest,
-    // Reddit,
-    // Salesforce,
-    // Slack,
-    // Spotify,
-    // Twitch,
-    // Twitter,
-    // Vipps({
-    //   issuer: "https://apitest.vipps.no/access-management-1.0/access/",
-    // }),
-    // WorkOS({ connection: process.env.AUTH_WORKOS_CONNECTION! }),
-    // Zoom,
-  
+      
   basePath: "/auth",
   session: { strategy: "jwt" },
   callbacks: {
